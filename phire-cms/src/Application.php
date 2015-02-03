89cd40298e1758933dc3fd22381f1ce17c3d59f6
Nick Sagona <nick@nolainteractive.com>
2015-02-03 08:58:44 -0600

    Patch sessionCheck method

--- phire-cms/src/Application.php
+++ phire-cms/src/Application.php
@@ -382,7 +382,7 @@ class Application extends \Pop\Application
         $sess      = $application->getService('session');
         $action    = $application->router()->getRouteMatch()->getAction();
         $route     = $application->router()->getRouteMatch()->getRoute();
-        $isInstall = (substr($route, 0, strlen(BASE_PATH . APP_URI . '/install')) == BASE_PATH . APP_URI . '/install');
+        $isInstall = (substr($route, 0, strlen(APP_URI . '/install')) == APP_URI . '/install');
 
         // Special install check
         if (isset($sess->app_uri) && (strpos($_SERVER['REQUEST_URI'], 'install/config') !== false)) {
